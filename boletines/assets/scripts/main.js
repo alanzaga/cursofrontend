bf78d94fe80455b8aead93c8e740f82f6e794679
Alan <zagabtmk@gmail.com>
2020-06-18 13:20:49 -0500

    redirreccionamiento desarrolloweb/boletines

--- boletines/assets/scripts/main.js
+++ boletines/assets/scripts/main.js
@@ -32,7 +32,7 @@ links.forEach(function(link){
 		content.classList.add("animate__animated");
 
 		setTimeout(function(){
-			location.href = "/boletines";
+			location.href = "cursofrontend/boletines";
 		}, 600);
 
 
