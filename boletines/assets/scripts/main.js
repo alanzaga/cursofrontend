40fd2a13fbd47d69b1b6fe9dfcd8c0d7a861c690
Alan <zagabtmk@gmail.com>
2020-06-18 12:29:01 -0500

    first commit

---
+++ boletines/assets/scripts/main.js
@@ -0,0 +1,42 @@
+
+
+/*let links = document.querySelectorAll("a");
+console.log(links);
+
+links.forEach(function(link){
+	console.log(link)
+})*/
+/*
+
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+	td.addEventListener('click', function(){
+		console.log(this);
+	})
+})
+*/
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+	link.addEventListener("click", function(ev){
+		ev.preventDefault();
+
+		let content = document.querySelector('.content');
+
+		content.classList.remove("animate__animated");
+		content.classList.remove("animate__fadeIn");
+
+		content.classList.add("animate__fadeOutUp");
+		content.classList.add("animate__animated");
+
+		setTimeout(function(){
+			location.href = "/";
+		}, 600);
+
+
+		return false;
+	});
+});
+
